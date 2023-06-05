9c878463fba41ed9209ed6989fed9757f5c46b5f
Taylor Burks <tayyy25@gmail.com>
2023-06-04 21:18:45 -0400

    parrot

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Cat {
+    //...
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`
+    }
+  }
+  
+  class Dog {
+    //...
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says woof!`
+  }
+}
+  
+  class Bird {
+    //...
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+  }
+
+  speak(bird) {
+    return `It's me! Pablo, the parrot!`
+  }
+
+  speak(bird2) {
+    return `${this.name} says squawk!`
+  }
+
+}
